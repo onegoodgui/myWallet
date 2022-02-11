c9d5c61b0ecb030e4e2532d6a98504cdba87a089
mateuskuritza <mateuskuritza@gmail.com>
2022-02-11 10:33:31 -0300

    Preparando revisão de código

--- src/services/loading.js
+++
@@ -1,9 +0,0 @@
-import {ThreeDots} from 'react-loader-spinner';
-
-function Loading({type, color, height, width, time, visible}){
-    return(
-        <ThreeDots type={type} color={color} height={height} width={width} timeout={time} visible={visible}/>
-    )
-}
-
-export default Loading;
